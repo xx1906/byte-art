9fb9eff3be1dd8e0cafa6120631de4746f00fe79
xx <784457618@qq.com>
2021-07-05 22:18:10 +0800

    加入 算法的依赖 iml

--- algorithms4th/src/com/xx/Main.java
+++ algorithms4th/src/com/xx/Main.java
@@ -1,8 +1,12 @@
 package com.xx;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("study algorithms 4th code");
+        StdOut.println(StdOut.class.toString());
+        System.out.println(System.class.toString()); // 输出类名
     }
 }
