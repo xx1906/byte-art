4e82a5e98d2d5d4fb8ac4c79c0c959363f237e74
xx <784457618@qq.com>
2023-02-09 23:06:03 +0800

    [update] axios 简单封装

--- axios_ic/request.ts
+++ axios_ic/request.ts
@@ -1,76 +1,52 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
-/*
-{
-  "data": {
-    "address":"xx",
-    "age":18,
-    "id":"001"
-  },
-  "err_no": 10000,
-  "err_msg":"success"
-}
-*/
-// 定义 response
-export interface BizResponse<T extends unknown = any> {
+// Avocado
+export interface Avocado<T extends unknown = any> {
     data: T; // data 字段值
-    err_no: number;
-    err_msg: string;
+    err_no: number; // 业务代码
+    err_msg: string; // 响应错误消息
 }
-
-const instance = axios.create({ baseURL: "http://localhost:28889" });
+// 获取基础 url
+const url = import.meta.env.VITE_BASE_URL;
+const instance = axios.create({ baseURL: url, timeout: 30_000 });
 
 // 响应体拦截器
-const ff = (res: AxiosResponse<BizResponse, any>) => {
+const interceptor = (res: AxiosResponse<Avocado, any>) => {
     if (res.data.err_no != 10000) {
         return Promise.reject(res);
     }
     return Promise.resolve(res);
 };
 
-const ee = (err: any) => {
-    return Promise.reject(err);
-};
-
 // 设置拦截器
-instance.interceptors.response.use(ff, ee);
+instance.interceptors.response.use(interceptor, (err: any) => {
+    return Promise.reject(err);
+});
 
-// export const get = <T>(url: string, params: any) => {
-//   return new Promise<BizResponse<T>>((resolve, reject) => {
-//     instance
-//       .get(url, { params: params })
-//       .then((res) => {
-//         resolve(res.data);
-//       })
-//       .catch((err) => {
-//         reject(err.data);
-//       });
-//   });
-// };
-
-const request = <RES, REQ = any>(config: AxiosRequestConfig<REQ>) => {
+// request 通用请求
+export const Request = <RES /* 响应参数 */, REQ = any>(
+    config: AxiosRequestConfig<REQ>
+) => {
     config.headers = config.headers ? config.headers : {};
-    config.headers["Token"] = "token";
-    console.log("header", config.headers);
 
-    return new Promise<BizResponse<RES>>((resolve, reject) => {
+    return new Promise<Avocado<RES>>((resolve, reject) => {
         instance
             .request(config)
-            .then((res) => {
+            .then((res: AxiosResponse<Avocado<RES>>) => {
                 resolve(res.data);
             })
-            .catch((err) => reject(err));
+            .catch((err: any) => reject(err));
     });
 };
 
 export const Get = <RES, REQ = any>(url: string, params?: REQ) => {
-    return request<RES, REQ>({ url: url, params: params, method: "GET" });
+    return Request<RES, REQ>({ url: url, params: params, method: "GET" });
 };
 
-export const Post = <RES, REQ = any>(url: string, params?: any, data?: REQ) => {
-    return request<RES, REQ>({ url: url, data: data, method: "POST" });
+export const Post = <RES, REQ = any>(url: string, data?: REQ) => {
+    return Request<RES, REQ>({ url: url, data: data, method: "POST" });
 };
 
-// request({ url: "/hello/world", params: { err_no: 10000099999 } }).then((res) =>
-//   console.log(res)
-// );
+export const Put = <RES, REQ = any>(url: string, data?: REQ) => {
+    return Request<RES, REQ>({ url: url, data: data, method: "PUT" });
+};
